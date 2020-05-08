e5767f138b5f8261283ea8ee93827f4ed7263472
Kang Yiming <kangyiming2006@gmail.com>
2020-05-08 11:19:27 +0800

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,5 @@
+username =#Your username
+password= #Your password
+stalk= #Username of person you are viewing
+xpos=1248 #Xcoordinate of a point in the scrollbar(In pyautogui coordinates)
+ypos=867 #Ycoordinate of a point in the scrollbar(In pyautogui coordinates)
